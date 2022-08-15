60c8d31c3236f81f2f0370263730678f25fe1abc
rookie <45302883+sucat1997@users.noreply.github.com>
2022-08-15 10:31:05 +0800

    提交 新增 从zip压缩包中解压一个指定的文件到指定的路径

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+      //解压rar测试
       UnCompressUtil.unRar5AndOver5("C:\\Users\\89755\\Desktop\\1h.第一季.rar","C:\\Users\\89755\\Desktop\\1h.第一季\\");
+
+      //解压zip压缩包中指定文件到指定路径测试
+      UnCompressUtil.decompress("A77AF133402D4B19B78897D95A2675AC.jpg","C:\\Users\\Administrator\\Desktop\\pg测试数据\\bcp\\aa.zip"
+      	,"C:\\Users\\Administrator\\Desktop\\pg测试数据\\back");
     }
 }
--- src/main/java/UnCompressUtil.java
+++ src/main/java/UnCompressUtil.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 
 public class UnCompressUtil {
 
+    /**
+     * 将rar格式的文件解压到指定路径
+     * @param rarDir 原压缩文件路径
+     * @param outputDir 解压目录
+     * @throws IOException
+     */
     public static void unRar5AndOver5(String rarDir,String outputDir) throws IOException {
         //RandomAccessFile randomAccessFile = null;
         BufferedRandomAccessFile randomAccessFile = null;
@@ -23,5 +29,59 @@ public class UnCompressUtil {
         inArchive.extract(in, false, new ExtractCallback(inArchive, "366", outputDir));
         inArchive.close();
         randomAccessFile.close();
+    }
+
+
+
+    /**
+     * 从zip压缩包中解压一个指定的文件到指定的路径
+     * @param targetFileName 目标文件名称
+     * @param srcPath 原压缩文件路径
+     * @param destPath 解压目录
+     * @throws Exception
+     */
+    public static void decompress(String targetFileName, String srcPath, String destPath) throws Exception {
+        File file = new File(srcPath);
+        if (!file.exists()) {
+            throw new RuntimeException(srcPath + "不存在");
+        }
+
+        ZipFile zf = new ZipFile(file);
+        Enumeration entries = zf.entries();
+        ZipEntry entry;
+        while (entries.hasMoreElements()) {
+            entry = (ZipEntry) entries.nextElement();
+            String name = entry.getName();
+            if (!name.endsWith(targetFileName)) {
+                continue;
+            }
+
+            File dir = new File(destPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            File f = new File(dir + File.separator + targetFileName);
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+
+            InputStream is = zf.getInputStream(entry);
+            FileOutputStream fos = new FileOutputStream(f);
+
+            int count;
+            byte[] buffer = new byte[1024 * 8];
+            while (true) {
+                count = is.read(buffer);
+                if (count == -1) {
+                    break;
+                }
+
+                fos.write(buffer, 0, count);
+            }
+
+            is.close();
+            fos.close();
+        }
     }
 }
